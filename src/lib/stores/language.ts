d2fe73c0dd3431683ab2bf6565dd4d191e30623d
Alex Bass <alexpjbass@gmail.com>
2023-01-03 14:46:24 +0100

    Set language from URL params

--- src/lib/stores/language.ts
+++ src/lib/stores/language.ts
@@ -6,7 +6,18 @@ const KEY = 'lang'
 
 export const activeLanguage = writable<Language>('de')
 
-if (browser) {
+function persistLanguage(language: Language) {
+	localStorage.setItem(KEY, language)
+}
+
+function determineLanguage() {
+	const urlLanguage = new URLSearchParams(window.location.search).get('lang')
+	if (urlLanguage && LANGUAGES.includes(urlLanguage)) {
+		activeLanguage.set(urlLanguage as Language)
+		persistLanguage(urlLanguage as Language)
+		return
+	}
+
 	const hasPersistedLanguage = localStorage.getItem(KEY)
 
 	// if the user has previously selected a language, use it
@@ -19,9 +30,16 @@ if (browser) {
 			activeLanguage.set(navigatorLanguage)
 		}
 	}
+}
 
+function persistLanguageOnChange() {
 	// update the language in local storage when it changes
 	activeLanguage.subscribe((language) => {
-		localStorage.setItem(KEY, language)
+		persistLanguage(language)
 	})
+}
+
+if (browser) {
+	determineLanguage()
+	persistLanguageOnChange()
 }
